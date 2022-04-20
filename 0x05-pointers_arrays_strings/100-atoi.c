9414a4440dca9717f8368bd59e279a6098a57312
iroatu08 <iroatu08@gmail.com>
2022-04-20 13:04:48 +0000

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,9 @@
 #include "main.h"
- /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
- */
+/**
+* _atoi - convert a string to an integer.
+* @s: char type string
+* Return: integer converted
+*/
 int _atoi(char *s)
 {
 int i;
