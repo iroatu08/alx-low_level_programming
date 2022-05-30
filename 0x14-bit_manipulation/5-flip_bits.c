c7277db594e1bd2da26d0287be905e9e7fe9f299
iroatu08 <iroatu08@gmail.com>
2022-05-30 23:29:12 +0100

    bit manipulation functions

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * flip_bits - find number of bits to flip from one number to another
+ * @n: first integer
+ * @m: second integer
+ * Return: (n_bits) number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit_difference;
+	unsigned int n_bits;
+
+	bit_difference = n ^ m;
+	n_bits = 0;
+
+	while (bit_difference)
+	{
+		if (bit_difference & 1)
+			n_bits++;
+
+		bit_difference >>= 1;
+	}
+
+	return (n_bits);
+}
